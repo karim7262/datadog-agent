fe0bcd443c291da580dd0f0be51a3689ba1900bc
Xavier Vello <xavier.vello@datadoghq.com>
2018-11-08 13:55:11 +0100

    fix integrations-core dep

--- tasks/go.py
+++ tasks/go.py
@@ -251,7 +251,7 @@ def deps(ctx, no_checks=False, core_dir=None, verbose=False, android=False):
             core_dir = os.path.join(os.getcwd(), 'vendor', 'integrations-core')
             checks_base = os.path.join(core_dir, 'datadog_checks_base')
             if not os.path.isdir(core_dir):
-                ctx.run('git clone -{} https://github.com/DataDog/integrations-core {}'.format(verbosity, core_dir))
+                ctx.run('git clone -b 6.6.x -{} https://github.com/DataDog/integrations-core {}'.format(verbosity, core_dir))
             ctx.run('pip install -{} {}'.format(verbosity, checks_base))
             ctx.run('pip install -{} -r {}'.format(verbosity, os.path.join(checks_base, 'requirements.in')))
 
