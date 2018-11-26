1f44d15c776ba670fa6f2974e831790e965f40b4
Xavier Vello <xavier.vello@datadoghq.com>
2018-11-26 14:16:14 +0100

    add TestStreamJSONMarshaler fix APIMetricType.UnmarshalText

--- pkg/metrics/series.go
+++ pkg/metrics/series.go
@@ -251,7 +251,7 @@ func encodePoints(ptr unsafe.Pointer, stream *jsoniter.Stream) {
 		} else {
 			needComa = true
 		}
-		fmt.Fprintf(stream, "[%v, %v]", int64(p.Ts), p.Value)
+		fmt.Fprintf(stream, "[%v,%v]", int64(p.Ts), p.Value)
 	}
 	stream.Write(jsonArrayEnd)
 }
