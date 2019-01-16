779a866d81cd83430b6d01688a16fcdd93a8f2f0
Gabriel Aszalos <gabriel.aszalos@datadoghq.com>
2019-01-16 10:17:52 -0500

    omnibus: update datadog-trace-agent config and add invoke task

--- pkg/trace/info/git_version.go
+++ pkg/trace/info/git_version.go
@@ -9,8 +9,8 @@ import (
 
 func init() {
 	Version = "0.99.0"
-	GitCommit = "90dcab81"
-	GitBranch = "HEAD"
-	BuildDate = "2019-01-08 10:02:22.993077 +0100 CET m=+0.007560824"
+	GitCommit = "cc5d7bb6"
+	GitBranch = "gbbr/hurray"
+	BuildDate = "2019-01-16 12:04:40.366984 -0500 EST m=+0.011782336"
 	GoVersion = strings.TrimPrefix(runtime.Version(), "go")
 }
