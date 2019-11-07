30b9dc5c86087d2db5a733339ac764b94a5a5012
Albert Vaca <albert.vaca@datadoghq.com>
2019-11-07 16:51:58 +0100

    Add go bin to gopath

--- tasks/system_probe.py
+++ tasks/system_probe.py
@@ -52,8 +52,13 @@ def build(ctx, race=False, go_version=None, incremental_build=False):
     ldflags, gcflags, env = get_build_flags(ctx)
 
     # extend PATH from gimme with the one from get_build_flags
-    if "PATH" in os.environ and "PATH" in goenv:
+    if "PATH" in goenv:
+        goenv["PATH"] += ":" + os.environ["GOPATH"]+"/bin"
+    else:
+        goenv["PATH"] = os.environ["GOPATH"]+"/bin"
+    if "PATH" in os.environ:
         goenv["PATH"] += ":" + os.environ["PATH"]
+
     env.update(goenv)
 
     # Add custom ld flags
