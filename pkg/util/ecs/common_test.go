cb2c5a6cf7f7ce6abda95b39be0edac0363a8d62
Xavier Lucas <xavier.lucas@datadoghq.com>
2019-11-07 17:00:56 +0100

    Add more test around ecs to internal object conversions

--- pkg/util/ecs/common_test.go
+++ pkg/util/ecs/common_test.go
@@ -12,9 +12,153 @@ import (
 	"testing"
 
 	"github.com/DataDog/datadog-agent/pkg/util/containers"
+	"github.com/DataDog/datadog-agent/pkg/util/containers/metrics"
 	v2 "github.com/DataDog/datadog-agent/pkg/util/ecs/metadata/v2"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestConvertMetaV2Container(t *testing.T) {
+	container := v2.Container{
+		CreatedAt:  "2018-02-01T20:55:10.554941919Z",
+		DockerID:   "43481a6ce4842eec8fe72fc28500c6b52edcc0917f105b83379f88cac1ff3946",
+		DockerName: "ecs-nginx-5-nginx-curl-ccccb9f49db0dfe0d901",
+		Image:      "nrdlngr/nginx-curl",
+		ImageID:    "sha256:2e00ae64383cfc865ba0a2ba37f61b50a120d2d9378559dcd458dc0de47bc165",
+		StartedAt:  "2018-02-01T20:55:11.064236631Z",
+		Limits: map[string]uint64{
+			"cpu":    0,
+			"memory": 0,
+		},
+	}
+	expected := &containers.Container{
+		CPULimit:    100,
+		Created:     1517518510,
+		EntityID:    "container_id://43481a6ce4842eec8fe72fc28500c6b52edcc0917f105b83379f88cac1ff3946",
+		ID:          "43481a6ce4842eec8fe72fc28500c6b52edcc0917f105b83379f88cac1ff3946",
+		Image:       "nrdlngr/nginx-curl",
+		ImageID:     "sha256:2e00ae64383cfc865ba0a2ba37f61b50a120d2d9378559dcd458dc0de47bc165",
+		Name:        "ecs-nginx-5-nginx-curl-ccccb9f49db0dfe0d901",
+		StartedAt:   1517518511,
+		Type:        "ECS",
+		AddressList: []containers.NetworkAddress{},
+	}
+	assert.Equal(t, expected, convertMetaV2Container(container))
+}
+
+func TestConvertMetaV2ContainerStats(t *testing.T) {
+	stats := &v2.ContainerStats{
+		CPU: v2.CPUStats{
+			System: 3951680000000,
+			Usage: v2.CPUUsage{
+				Kernelmode: 2260000000,
+				Total:      9743590394,
+				Usermode:   7450000000,
+			},
+		},
+		Memory: v2.MemStats{
+			Details: v2.DetailedMem{
+				RSS:     1564672,
+				Cache:   65499136,
+				PgFault: 430478,
+			},
+			Limit:    268435456,
+			MaxUsage: 139751424,
+			Usage:    77254656,
+		},
+		IO: v2.IOStats{
+			BytesPerDeviceAndKind: []v2.OPStat{
+				{
+					Kind:  "Read",
+					Major: 259,
+					Minor: 0,
+					Value: 12288,
+				},
+				{
+					Kind:  "Write",
+					Major: 259,
+					Minor: 0,
+					Value: 144908288,
+				},
+				{
+					Kind:  "Sync",
+					Major: 259,
+					Minor: 0,
+					Value: 8122368,
+				},
+				{
+					Kind:  "Async",
+					Major: 259,
+					Minor: 0,
+					Value: 136798208,
+				},
+				{
+					Kind:  "Total",
+					Major: 259,
+					Minor: 0,
+					Value: 144920576,
+				},
+			},
+			OPPerDeviceAndKind: []v2.OPStat{
+				{
+					Kind:  "Read",
+					Major: 259,
+					Minor: 0,
+					Value: 3,
+				},
+				{
+					Kind:  "Write",
+					Major: 259,
+					Minor: 0,
+					Value: 1618,
+				},
+				{
+					Kind:  "Sync",
+					Major: 259,
+					Minor: 0,
+					Value: 514,
+				},
+				{
+					Kind:  "Async",
+					Major: 259,
+					Minor: 0,
+					Value: 1107,
+				},
+				{
+					Kind:  "Total",
+					Major: 259,
+					Minor: 0,
+					Value: 1621,
+				},
+			},
+			ReadBytes:  1024,
+			WriteBytes: 256,
+		},
+		Network: v2.NetStats{},
+	}
+
+	expectedCPU := metrics.CgroupTimesStat{
+		User:        7450000000,
+		System:      2260000000,
+		SystemUsage: 3951680000000,
+	}
+	expectedMem := metrics.CgroupMemStat{
+		Cache:           65499136,
+		MemUsageInBytes: 77254656,
+		Pgfault:         430478,
+		RSS:             1564672,
+	}
+	expectedIO := metrics.CgroupIOStat{
+		ReadBytes:  1024,
+		WriteBytes: 256,
+	}
+
+	cpu, mem, io, memLimit := convertMetaV2ContainerStats(stats)
+
+	assert.Equal(t, expectedCPU, cpu)
+	assert.Equal(t, expectedMem, mem)
+	assert.Equal(t, expectedIO, io)
+	assert.Equal(t, uint64(268435456), memLimit)
+}
 
 func TestParseContainerNetworkAddresses(t *testing.T) {
 	ports := []v2.Port{
