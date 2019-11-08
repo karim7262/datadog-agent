e19bce892c54e60aa214312ae34cd27b25c102bc
Arthur Bellal <arthur.bellal@datadoghq.com>
2019-11-08 10:54:40 +0100

    avoid blocking on aggregator

--- pkg/aggregator/aggregator.go
+++ pkg/aggregator/aggregator.go
@@ -194,9 +194,9 @@ type BufferedAggregator struct {
 // NewBufferedAggregator instantiates a BufferedAggregator
 func NewBufferedAggregator(s serializer.MetricSerializer, hostname, agentName string, flushInterval time.Duration) *BufferedAggregator {
 	aggregator := &BufferedAggregator{
-		bufferedMetricIn:       make(chan []*metrics.MetricSample, 100), // TODO make buffer size configurable
-		bufferedServiceCheckIn: make(chan []*metrics.ServiceCheck, 100), // TODO make buffer size configurable
-		bufferedEventIn:        make(chan []*metrics.Event, 100),        // TODO make buffer size configurable
+		bufferedMetricIn:       make(chan []*metrics.MetricSample, 8192), // TODO make buffer size configurable
+		bufferedServiceCheckIn: make(chan []*metrics.ServiceCheck, 100),  // TODO make buffer size configurable
+		bufferedEventIn:        make(chan []*metrics.Event, 100),         // TODO make buffer size configurable
 
 		metricIn:       make(chan *metrics.MetricSample, 100), // TODO make buffer size configurable
 		serviceCheckIn: make(chan metrics.ServiceCheck, 100),  // TODO make buffer size configurable
