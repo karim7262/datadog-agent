ce082722bfc5a8e51a753d9932778c5c2863f5d3
Olivier G <olivier.gaca@datadoghq.com>
2020-01-31 11:36:54 +0100

    Fix conflicts

--- tasks/agent.py
+++ tasks/agent.py
@@ -84,8 +84,7 @@ PUPPY_CORECHECKS = [
 def build(ctx, rebuild=False, race=False, build_include=None, build_exclude=None,
           puppy=False, development=True, precompile_only=False, skip_assets=False,
           embedded_path=None, rtloader_root=None, python_home_2=None, python_home_3=None,
-          major_version='7', arch='x64', exclude_rtloader=False,
-          python_runtimes=None):
+          major_version='7', python_runtimes='3', arch='x64', exclude_rtloader=False):
     """
     Build the agent. If the bits to include in the build are not specified,
     the values from `invoke.yaml` will be used.
