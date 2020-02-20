7c605b92e4c0d52a47181313ec18226261b8f443
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-02-20 10:12:34 +0100

    Add CFLAGS and LDFLAGS to aerospike build

--- omnibus/config/software/aerospike-py3.rb
+++ omnibus/config/software/aerospike-py3.rb
@@ -8,6 +8,8 @@ build do
   env = {
     "DOWNLOAD_C_CLIENT" => "0",
     "AEROSPIKE_C_HOME" => "#{install_dir}/embedded/lib/aerospike",
+    "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
   }
 
   command "#{install_dir}/embedded/bin/pip3 install --no-binary :all: aerospike==#{version}", :env => env
