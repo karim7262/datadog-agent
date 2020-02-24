cb87b90da38cf7501bfdfe67fc62b4996ee5a6fc
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-02-24 16:03:58 +0100

    [kitchen] Work around SLES tests failing because of network interface restarts

---
+++ test/kitchen/site-cookbooks/dd-agent-sles-workaround/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: dd-agent-sles-workaround
+# Recipe:: default
+#
+# Copyright (C) 2020 Datadog
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if node['platform_family'] == 'suse'
+  # Stop the Windows Azure Agent, for some reason it's changing the hostname
+  # every 30s on SLES 11 and 12, which leads to a network interface reset,
+  # making it likely for tests to fail if a network call happens during the reset.
+  service 'waagent' do
+    service_name "waagent"
+    action :stop
+  end
+end
