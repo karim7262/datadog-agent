9777a38e5ca6f4d8006427612402536125f6045e
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-02-25 10:43:11 +0100

    Test modifying waagent conf

--- test/kitchen/site-cookbooks/dd-agent-sles-workaround/recipes/default.rb
+++ test/kitchen/site-cookbooks/dd-agent-sles-workaround/recipes/default.rb
@@ -8,11 +8,20 @@
 #
 
 if node['platform_family'] == 'suse'
+  execute 'update Azure Agent conf' do
+    command "sed -i 's/Provisioning\\.MonitorHostName=y/Provisioning\\.MonitorHostName=n/' /etc/waagent.conf"
+  end
+
   # Stop the Windows Azure Agent, for some reason it's changing the hostname
   # every 30s on SLES 11 and 12, which leads to a network interface reset,
   # making it likely for tests to fail if a network call happens during the reset.
   service 'waagent' do
     service_name "waagent"
-    action :stop
+    action :restart
+  end
+
+  # Put eth0 back up in case the waagent was taking it down while we restarted it
+  execute 'bring eth0 up' do
+    command "/sbin/ifup eth0"
   end
 end
