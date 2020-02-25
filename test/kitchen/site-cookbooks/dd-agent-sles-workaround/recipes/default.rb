d6a87f45ead6187d4c05b971428ed0b62421d10f
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-02-25 08:50:59 +0100

    Test

--- test/kitchen/site-cookbooks/dd-agent-sles-workaround/recipes/default.rb
+++ test/kitchen/site-cookbooks/dd-agent-sles-workaround/recipes/default.rb
@@ -8,11 +8,19 @@
 #
 
 if node['platform_family'] == 'suse'
+  execute 'sleep' do
+    command "sleep 5"
+  end
+
   # Stop the Windows Azure Agent, for some reason it's changing the hostname
   # every 30s on SLES 11 and 12, which leads to a network interface reset,
   # making it likely for tests to fail if a network call happens during the reset.
   service 'waagent' do
     service_name "waagent"
     action :stop
+  end
+
+  execute 'sleep' do
+    command "sleep 5"
   end
 end
