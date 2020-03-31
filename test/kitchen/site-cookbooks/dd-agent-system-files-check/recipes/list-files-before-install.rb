517987a8314be71b8684266d36909fddc707c38a
Albert Vaca <albert.vaca@datadoghq.com>
2020-03-31 13:48:09 +0200

    Kitchen system files check

---
+++ test/kitchen/site-cookbooks/dd-agent-system-files-check/recipes/list-files-before-install.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: dd-agent-system-files-check
+# Recipe:: default
+#
+# Copyright (C) 2020 Datadog
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if node['platform_family'] != 'windows'
+    puts "dd-agent-system-files-check: Not implemented on non-windows"
+else
+    ruby_block "list-before-files" do
+        block do
+            File.open("C:/before-files.txt", "w") do |out|
+                Dir.glob("C:/windows/**/*").each { |file| out.puts(file) }
+            end
+        end
+        action :run
+    end
+end
