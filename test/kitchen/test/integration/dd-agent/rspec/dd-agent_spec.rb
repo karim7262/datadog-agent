8e338e1ca3782ecbbd6c8ddfc818687e27d55342
Albert Vaca <albert.vaca@datadoghq.com>
2020-04-06 13:51:18 +0200

    Test system files are there

--- test/kitchen/test/integration/dd-agent/rspec/dd-agent_spec.rb
+++ test/kitchen/test/integration/dd-agent/rspec/dd-agent_spec.rb
@@ -3,3 +3,27 @@ require 'spec_helper'
 describe 'dd-agent' do
   include_examples 'Agent'
 end
+
+
+if os == :windows
+
+  describe 'system-files-intact' do
+
+    before_files = File.readlines('c:/before-files.txt')
+    after_files = File.readlines('c:/after-files.txt')
+
+    missing_files = before_files - after_files
+    new_files = after_files - before_files
+
+    puts "New files:"
+    new_files.each { |f| puts(f) }
+
+    puts "Missing files:"
+    missing_files.each { |f| puts(f) }
+
+    expect(missing_files).to be_empty
+    expect(new_files).to be_empty
+
+  end
+
+end
