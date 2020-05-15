21586f23fbba5619b88b79bc3c97081a470b8dde
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2020-05-15 16:52:24 -0400

    comment rename

--- rtloader/include/rtloader_types.h
+++ rtloader/include/rtloader_types.h
@@ -125,7 +125,7 @@ typedef void (*cb_set_external_tags_t)(char *, char *, char **);
 typedef void (*cb_write_persistent_cache_t)(char *, char *);
 // (value)
 typedef char *(*cb_read_persistent_cache_t)(char *);
-// (sql_query, error)
+// (sql_query, error_message)
 typedef char *(*cb_obfuscate_sql_t)(char *, char **);
 
 // _util
